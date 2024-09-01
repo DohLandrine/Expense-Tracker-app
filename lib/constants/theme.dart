2fe1b27a986e0c0d291902af59486f0d183e44b3
dohlandrine <landrinedoh22@gmail.com>
2024-09-01 22:20:06 +0100

    creaed the custom themes for my app created the custom snackbar widget, all available in the constants folder

---
+++ lib/constants/theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Colour {
+  static const dark = Colors.black;
+  static const white = Colors.white;
+  static const blue = Colors.blue;
+  static const lightDark = Color.fromARGB(95, 186, 179, 179);
+  static const darkBlue = Color.fromARGB(255, 20, 91, 150);
+  static const green = Colors.green;
+  static const red = Color.fromARGB(255, 223, 97, 88);
+}
